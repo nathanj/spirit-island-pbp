20f458835497c6efe7ece56ccb75de0c16737516
Nathan Jones <nathanj439@gmail.com>
2022-02-07 13:13:49 -0500

    add logging when debug=false

--- island/settings.py
+++ island/settings.py
@@ -157,3 +157,18 @@ INTERNAL_IPS = [
 #    }
 #}
 
+
+if DEBUG == False:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+            },
+        },
+        "loggers": {
+            "django": {"handlers": ["console"], "level": "INFO"},
+        },
+    }
+
