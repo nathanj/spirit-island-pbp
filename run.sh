4b0258f0b4c2dca965a5f6260514fa2761f087d6
Nathan Jones <nathanj439@gmail.com>
2024-03-29 09:32:29 -0400

    fix to work with old poetry versions

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PYTHONUNBUFFERED=TRUE
 PATH=$PATH:/home/ubuntu/.local/bin
-poetry install
+poetry install --no-root
 poetry run python3 ./manage.py collectstatic --noinput
 poetry run python3 ./manage.py migrate
 exec poetry run gunicorn island.wsgi
