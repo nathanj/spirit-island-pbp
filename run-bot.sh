6f95ed03d1a9493f2e47d73fbe6f5d602d8a32c4
Nathan Jones <nathaniel.jones@oracle.com>
2024-09-09 10:05:47 -0400

    run as different user

--- run-bot.sh
+++ run-bot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PYTHONUNBUFFERED=TRUE
-PATH=/home/ubuntu/.local/bin:$PATH
+PATH=/home/si/.local/bin:$PATH
 poetry --version
 poetry install --no-root
 exec poetry run python3 ./bot.py
