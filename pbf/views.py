c0027605e36d93f9fb02dafaa08b2ab90a18e225
Peter Tseng <pht24@cornell.edu>
2025-03-18 18:42:39 +0000

    extract common code between gain/take/host draw to cards_from_deck

--- pbf/views.py
+++ pbf/views.py
@@ -443,18 +443,27 @@ def draw_cards(request, game_id, type):
     if cards_needed <= 0:
         return render(request, 'host_draw.html', {'msg': f"Can't draw {cards_needed} cards"})
 
+    cards_drawn = cards_from_deck(game, cards_needed, type)
+    game.discard_pile.add(*cards_drawn)
+
+    draw_result = f"drew {len(cards_drawn)} {type} power card{'s' if len(cards_drawn) != 1 else ''}"
+    draw_result_explain = "" if len(cards_drawn) == cards_needed else f" (there were not enough cards to draw all {cards_needed})"
+    card_names = ', '.join(card.name for card in cards_drawn)
+
+    add_log_msg(game, text=f'Host {draw_result}: {card_names}', images=",".join('./pbf/static' + card.url() for card in cards_drawn))
+
+    return render(request, 'host_draw.html', {'msg': f"You {draw_result}{draw_result_explain}: {card_names}", 'cards': cards_drawn})
+
+def cards_from_deck(game, cards_needed, type):
     if type == 'minor':
         deck = game.minor_deck
     elif type == 'major':
         deck = game.major_deck
     else:
-        # don't need to render host_draw;
-        # the form should never submit an invalid type in the first place
         raise ValueError(f"can't draw from {type} deck")
 
     cards_have = deck.count()
 
-    draw_result_explain = ''
     if cards_have >= cards_needed:
         cards_drawn = sample(list(deck.all()), cards_needed)
         deck.remove(*cards_drawn)
@@ -470,17 +479,9 @@ def draw_cards(request, game_id, type):
             deck.remove(*new_cards)
         else:
             cards_drawn.extend(list(deck.all()))
-            draw_result_explain = f" (there were not enough cards to draw all {cards_needed})"
             deck.clear()
 
-    game.discard_pile.add(*cards_drawn)
-
-    draw_result = f"drew {len(cards_drawn)} {type} power card{'s' if len(cards_drawn) != 1 else ''}"
-    card_names = ', '.join(card.name for card in cards_drawn)
-
-    add_log_msg(game, text=f'Host {draw_result}: {card_names}', images=",".join('./pbf/static' + card.url() for card in cards_drawn))
-
-    return render(request, 'host_draw.html', {'msg': f"You {draw_result}{draw_result_explain}: {card_names}", 'cards': cards_drawn})
+    return cards_drawn
 
 def reshuffle_discard(game, type):
     if type == 'minor':
@@ -498,21 +499,11 @@ def reshuffle_discard(game, type):
 
 def take_power(request, player_id, type):
     player = get_object_or_404(GamePlayer, pk=player_id)
-    if type == 'minor':
-        deck = player.game.minor_deck
-    else:
-        deck = player.game.major_deck
 
-    cards = list(deck.all())
-    if not cards:
-        # deck was empty
-        reshuffle_discard(player.game, type)
-        cards = list(deck.all())
-    shuffle(cards)
-    card = cards[0]
-    player.hand.add(card)
-    deck.remove(card)
+    taken_cards = cards_from_deck(player.game, 1, type)
+    player.hand.add(*taken_cards)
 
+    card = taken_cards[0]
     add_log_msg(player.game, text=f'{player.circle_emoji} {player.spirit.name} takes {card.name}', images='./pbf/static/' + card.url())
 
     compute_card_thresholds(player)
@@ -534,28 +525,8 @@ def gain_healing(request, player_id):
 
 def gain_power(request, player_id, type, num):
     player = get_object_or_404(GamePlayer, pk=player_id)
-    if type == 'minor':
-        deck = player.game.minor_deck
-    else:
-        deck = player.game.major_deck
 
-    cards = list(deck.all())
-    shuffle(cards)
-    selection = cards[:num]
-    for c in selection:
-        deck.remove(c)
-
-    # handle power deck running out
-    if len(selection) != num:
-        reshuffle_discard(player.game, type)
-
-        cards = list(deck.all())
-        shuffle(cards)
-        selection2 = cards[:num-len(selection)]
-        for c in selection2:
-            deck.remove(c)
-        selection += selection2
-
+    selection = cards_from_deck(player.game, num, type)
     player.selection.set(selection)
 
     cards_str = ", ".join([str(card) for card in selection])
