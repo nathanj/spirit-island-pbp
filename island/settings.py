1cdc81ae19acb61a6b197815e17b2c6fb54963c5
Roscoe <roscoejp@gmail.com>
2025-03-20 18:00:11 -0400

    allow secret key to be set via environment variable
    Previously there was no way to set it at all

--- island/settings.py
+++ island/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$1nkg1t+jd(&$u1-!-hejbk)#)!x#m=%ly_i)pgwhg)h0qt!2k'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') or 'django-insecure-$1nkg1t+jd(&$u1-!-hejbk)#)!x#m=%ly_i)pgwhg)h0qt!2k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') == 'yes'
