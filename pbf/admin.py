4c81922446599ea6995741423c5a6511732c2dc3
Peter Tseng <pht24@cornell.edu>
2025-06-18 14:55:12 +0000

    admin: Add searches for game (name or ID) and player (name or game ID)
    There aren't indexes on the name fields so not really sure how these
    will perform.

--- pbf/admin.py
+++ pbf/admin.py
@@ -9,6 +9,8 @@ class CardAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 class GameAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'name')
+    search_help_text = 'Search by ID or name'
     list_display = ('id', 'created_at', 'name', 'discord_channel')
     ordering = ('-created_at', )
     filter_horizontal = ('minor_deck', 'major_deck', 'discard_pile')
@@ -16,6 +18,8 @@ class GameAdmin(admin.ModelAdmin):
         return False
 
 class GamePlayerAdmin(admin.ModelAdmin):
+    search_fields = ('game__id', 'name')
+    search_help_text = 'Search by game ID or player name'
     list_display = ('id', 'game', 'spirit', 'name')
     autocomplete_fields = ('hand', 'discard', 'play', 'selection', 'days')
 
