d88d931003d6d2151244df8289788954d2b6dbf1
Peter Tseng <pht24@cornell.edu>
2025-07-14 08:44:01 +0000

    only install main in production
    We don't need to install any dev dependencies, for example.
    In particular, we've just moved debug-toolbar to dev dependency.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 export PYTHONUNBUFFERED=TRUE
 PATH=/home/si/.local/bin:$PATH
 poetry --version
-poetry install --no-root
+poetry install --no-root --only main
 poetry run python3 ./manage.py collectstatic --noinput
 poetry run python3 ./manage.py migrate
 exec poetry run gunicorn island.wsgi
