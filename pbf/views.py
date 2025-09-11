dd11049db3ad2c089188d2a09851aa3e3b377e7f
Sara Joiner <sjoiner@microsoft.com>
2025-09-10 22:47:34 -0700

    Add log message for spirit-specific elements (#288)
    (Wounded Waters' healing markers and Memory's prepared elements)

--- pbf/views.py
+++ pbf/views.py
@@ -1177,9 +1177,12 @@ def add_impending_log_msgs(player):
             add_log_msg(player.game, player=player, text=f'adjusts energy on impended card {card.name} ({impended_card_with_energy.energy}/{impended_card_with_energy.cost_with_scenario})')
 
 def add_spirit_specific_resource_msgs(player):
-    # TODO: Add support for logging spirit-specific elements for Memory and Wounded Waters
     if player.spirit_specific_resource_elements() is None:
         add_log_msg(player.game, player=player, text=f'has {player.spirit_specific_resource} {player.spirit_specific_resource_name()}')
+    else:
+        element_msg = ", ".join(f'{current} {elt}' for plus, minus, current, elt in player.spirit_specific_resource_elements() if current > 0)
+        if element_msg:
+            add_log_msg(player.game, player=player, text=f'{player.spirit_specific_resource_name()}: {element_msg}')
 
 def change_energy(request, player_id, amount):
     amount = int(amount)
