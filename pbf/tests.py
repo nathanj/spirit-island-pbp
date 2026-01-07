7b6f729f00fd0c8159fb1fe4f2421253fbdc92ab
Peter Tseng <pht24@cornell.edu>
2026-01-07 07:34:45 +0000

    add Second Wave scenario
    closes https://github.com/nathanj/spirit-island-pbp/issues/311

--- pbf/tests.py
+++ pbf/tests.py
@@ -1310,6 +1310,20 @@ class TestScenario(TestCase):
         self.assertEqual(majors_before - 1, game.major_deck.count())
         self.assertNotIn(card, game.major_deck.all())
 
+    def test_add_unique(self):
+        client, game, player = self.setup_game()
+
+        card = Card.objects.get(name='Boon of Vigor')
+        minors_before = game.minor_deck.count()
+        majors_before = game.major_deck.count()
+        self.assertEqual(0, player.scenario.count())
+
+        client.post(f"/game/{player.id}/add_to_scenario/{card.id}")
+
+        self.assertEqual(['Boon of Vigor'], list(player.scenario.values_list('name', flat=True)))
+        self.assertEqual(minors_before, game.minor_deck.count())
+        self.assertEqual(majors_before, game.major_deck.count())
+
     def test_gain_scenario(self):
         client, game, player = self.setup_game()
 
@@ -1323,6 +1337,20 @@ class TestScenario(TestCase):
         self.assertEqual(['Call to Ferocity'], list(player.scenario.values_list('name', flat=True)))
         self.assertEqual(hand_before + 1, player.hand.count())
         self.assertIn(card1, player.hand.all())
+
+    def test_discard_scenario(self):
+        client, game, player = self.setup_game()
+
+        card1 = Card.objects.get(name='Call to Isolation')
+        card2 = Card.objects.get(name='Call to Ferocity')
+        client.post(f"/game/{player.id}/add_to_scenario/{card1.id}")
+        client.post(f"/game/{player.id}/add_to_scenario/{card2.id}")
+        discard_before = game.discard_pile.count()
+
+        client.post(f"/game/{player.id}/discard_scenario/{card1.id}")
+        self.assertEqual(['Call to Ferocity'], list(player.scenario.values_list('name', flat=True)))
+        self.assertEqual(discard_before + 1, game.discard_pile.count())
+        self.assertIn(card1, game.discard_pile.all())
 
 class TestDaysThatNeverWere(TestCase):
     def test_create(self):
--- pbf/views.py
+++ pbf/views.py
@@ -971,6 +971,8 @@ def setup_deck(request: HttpRequest, player_id: int, type: str) -> HttpResponse:
         cards = player.game.minor_deck.all()
     elif type == 'major':
         cards = player.game.major_deck.all()
+    elif type == 'unique':
+        cards = Card.objects.filter(type=Card.UNIQUE)
     else:
         raise ValueError(f"invalid card type")
 
@@ -1001,6 +1003,9 @@ def add_to_scenario(request: HttpRequest, player_id: int, card_id: int) -> HttpR
         deck: 'Card_ManyRelatedManager[Any]' = player.game.minor_deck
     elif card.type == Card.MAJOR:
         deck = player.game.major_deck
+    elif card.type == Card.UNIQUE:
+        player.scenario.add(card)
+        return render(request, 'power_deck_setup.html', {'name': 'Unique', 'player': player, 'cards': Card.objects.filter(type=Card.UNIQUE)})
     else:
         raise ValueError(f"Can't add {card}")
 
@@ -1017,6 +1022,16 @@ def gain_scenario(request: HttpRequest, player_id: int, card_id: int) -> HttpRes
     player.scenario.remove(card)
 
     add_log_msg(player.game, player=player, text=f'gains {card.name} from their Destiny')
+
+    return with_log_trigger(render(request, 'player.html', {'player': player}))
+
+def discard_scenario(request: HttpRequest, player_id: int, card_id: int) -> HttpResponse:
+    player = get_object_or_404(GamePlayer, pk=player_id)
+    card = get_object_or_404(player.scenario, pk=card_id)
+    player.scenario.remove(card)
+    player.game.discard_pile.add(card)
+
+    add_log_msg(player.game, text=f'{card.name} (Second Wave) discarded')
 
     return with_log_trigger(render(request, 'player.html', {'player': player}))
 
