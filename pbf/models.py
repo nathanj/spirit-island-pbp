a377de3431cbaaf159b53dd0b59ebca1ef49536a
Peter Tseng <pht24@cornell.edu>
2026-01-12 03:02:20 +0000

    check_elements: remove unnecessary list in `any`
    passing a generator is beneficial because `any` short-circuits, so we
    may avoid evaluating later elements if an earlier element already gives
    us the answer.

--- pbf/models.py
+++ pbf/models.py
@@ -11,7 +11,7 @@ def chunk(str, n):
 
 def check_elements(elements, desired, equiv_elements=None):
     if type(desired) == type([]):
-        return any([check_elements(elements, d) for d in desired])
+        return any(check_elements(elements, d) for d in desired)
 
     chunks = chunk(desired, 2)
     if equiv_elements:
