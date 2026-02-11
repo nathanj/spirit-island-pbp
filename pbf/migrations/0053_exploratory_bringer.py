f07230f1e7a6f5adbb16982b349c2a25e911f297
Peter Tseng <pht24@cornell.edu>
2026-02-11 07:51:03 +0000

    migrations/0053_exploratory_bringer: only show nonzero player counts
    don't want to see the '0 players and 0 cards' message each time we run
    the tests

--- pbf/migrations/0053_exploratory_bringer.py
+++ pbf/migrations/0053_exploratory_bringer.py
@@ -1,13 +1,5 @@
 from django.db import migrations, models
 
-def count_exploratory_bringer(apps, schema_editor):
-    GamePlayer = apps.get_model('pbf', 'GamePlayer')
-    Card = apps.get_model('pbf', 'Card')
-
-    players = GamePlayer.objects.filter(spirit__name='Exploratory Bringer').count()
-    cards = Card.objects.filter(spirit__name='Exploratory Bringer').count()
-    print(f"{players} players {cards} cards using Exploratory Bringer")
-
 def convert_exploratory_bringer(apps, schema_editor):
     GamePlayer = apps.get_model('pbf', 'GamePlayer')
     Spirit = apps.get_model('pbf', 'Spirit')
@@ -15,7 +7,9 @@ def convert_exploratory_bringer(apps, schema_editor):
     bringer = Spirit.objects.get(name='Bringer')
     players = GamePlayer.objects.filter(spirit__name='Exploratory Bringer')
 
-    players.update(spirit=bringer, aspect='Exploratory')
+    updated = players.update(spirit=bringer, aspect='Exploratory')
+    if updated:
+        print(f"{updated} players converted from Exploratory Bringer")
 
 def delete_exploratory_bringer(apps, schema_editor):
     GamePlayer = apps.get_model('pbf', 'GamePlayer')
@@ -39,7 +33,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-            migrations.RunPython(count_exploratory_bringer, migrations.RunPython.noop),
             migrations.RunPython(convert_exploratory_bringer, migrations.RunPython.noop),
             #migrations.RunPython(delete_exploratory_bringer, add_exploratory_bringer),
             migrations.RunPython(delete_exploratory_bringer, migrations.RunPython.noop),
